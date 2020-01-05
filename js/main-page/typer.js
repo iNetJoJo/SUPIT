7b58a7779c40daf8e5b1954a3eaa3e1eb8dc95f3
Marin Miletić <mrnmiletic@gmail.com>
2020-01-05 03:05:05 +0100

    add news page, remove unnecessary jquery local file

--- js/main-page/typer.js
+++ js/main-page/typer.js
@@ -1,29 +1,29 @@
-var text = 'Budi izvrstan u onome sto vidis!';
-var i = 0;
+$(document).ready(function() {
+    var text = 'Budi izvrstan u onome sto volis!';
+    var i = 0;
 
-var typeTimings = [];
+    var typeTimings = [];
 
-function generateTypingTimings(){
-    for (let j = 0; j < text.length; j++) {
-        let randomNumber = Math.floor(Math.random() * 500);
-        typeTimings.push(randomNumber);
+    function generateTypingTimings(){
+        for (let j = 0; j < text.length; j++) {
+            let randomNumber = Math.floor(Math.random() * 500);
+            typeTimings.push(randomNumber);
+        }
     }
-}
 
-generateTypingTimings();
-
-function Typer() {
-    let currentText = $("#inputText").text();
-    console.log(currentText);
+    function Typer() {
+        let currentText = $("#inputText").text();
+        console.log(currentText);
 
         $("#inputText").append(text[i]);
 
         i++;
-    if( i < text.length ){
-        setTimeout( Typer,typeTimings[i]);
+        if( i < text.length ){
+            setTimeout( Typer,typeTimings[i]);
+        }
     }
-}
 
-Typer();
 
-console.log(';tha0ugrhds9uasdg9saydg');
+    generateTypingTimings();
+    Typer();
+});
