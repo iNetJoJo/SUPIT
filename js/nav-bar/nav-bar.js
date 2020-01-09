6d514882abe7be669d3c99d8007b516f5ad3ae70
Marin Miletić <mrnmiletic@gmail.com>
2020-01-09 01:10:14 +0100

    Finished desktop version
    Summary:
     * implementing tablet and smartphone responsivness WIP
    
     For future:
      * sometimes images and buttons glitch out for few miliseconds on reload

--- js/nav-bar/nav-bar.js
+++ js/nav-bar/nav-bar.js
@@ -2,31 +2,47 @@ var isShown = false;
 var firstTimeLoad = true;
 var nav_container =$('.navigation-container');
 var nav_buttons = $('.navigation-left');
+var nav_right = $('.nav-item-r');
 
 $('html').click(function() {
-    isShown = false;
-if (!firstTimeLoad) {
-    nav_container.removeClass('nav-bar-animate-in');
-    nav_container.addClass('nav-bar-animate-out');
-    nav_buttons.css('display', 'none');
-}
-firstTimeLoad = false;
+    console.log(window.innerWidth, window.innerHeight);
+    if(window.innerWidth < 1200){
+        isShown = false;
+        if (!firstTimeLoad) {
+            nav_container.removeClass('nav-bar-animate-in');
+            nav_container.addClass('nav-bar-animate-out');
+            nav_buttons.css('display', 'none');
+        }
+        firstTimeLoad = false;
+    }
 });
 
-$('.navigation-container').click(function(event){
+nav_container.click(function(event){
     console.log($(this).width(), 'yeet');
+    if(window.innerWidth < 1200){
+        if (!isShown){
+            nav_container.addClass('nav-bar-animate-in');
+            nav_container.removeClass('nav-bar-animate-out');
+            nav_buttons.addClass('navigation-left-slidein');
+            nav_buttons.css('display', 'grid');
+        }
+        else {
+            nav_container.removeClass('nav-bar-animate-in');
+            nav_container.addClass('nav-bar-animate-out');
+            nav_buttons.css('display', 'none');
+        }
+        isShown = !isShown;
+        event.stopPropagation();
+    }
+});
 
-    if (!isShown){
-        nav_container.addClass('nav-bar-animate-in');
-        nav_container.removeClass('nav-bar-animate-out');
-        nav_buttons.addClass('navigation-left-slidein');
+$(window).resize(function(){
+    console.log('yeet');
+    if(window.innerWidth > 1200){
         nav_buttons.css('display', 'grid');
     }
-    else {
-        nav_container.removeClass('nav-bar-animate-in');
-        nav_container.addClass('nav-bar-animate-out');
+    else{
         nav_buttons.css('display', 'none');
     }
-    isShown = !isShown;
-    event.stopPropagation();
+
 });
